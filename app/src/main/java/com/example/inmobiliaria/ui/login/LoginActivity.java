07962149085c80071ddcae8c406e91410ab9ca14
Daisfe16 <daisfe15@gmail.com>
2025-10-13 01:44:28 -0300

    Version 2

--- app/src/main/java/com/example/inmobiliaria/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/inmobiliaria/ui/login/LoginActivity.java
@@ -3,10 +3,13 @@ package com.example.inmobiliaria.ui.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.example.inmobiliaria.MainActivity;
 import com.example.inmobiliaria.databinding.ActivityLoginBinding;
+import com.example.inmobiliaria.ui.request.ApiClient;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -17,6 +20,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        String tokenGuardado = ApiClient.obtenerToken(this);
+        if (tokenGuardado != null && !tokenGuardado.isEmpty()) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            return;
+        }
 
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -27,14 +37,18 @@ public class LoginActivity extends AppCompatActivity {
 
         loginViewModel.getTokenLiveData().observe(this, token -> {
             Toast.makeText(this, "Login exitoso ✅", Toast.LENGTH_SHORT).show();
-            // Acá podrías abrir otra Activity o guardar más datos
+
+            // Ir a MainActivity
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
         });
 
         loginViewModel.getErrorLiveData().observe(this, error -> {
             Toast.makeText(this, error, Toast.LENGTH_SHORT).show();
         });
 
-        // Evento del botón
+        // Evento del botón igresar
         binding.btnLogin.setOnClickListener(v -> {
             String usuario = binding.etUsuario.getText().toString();
             String clave = binding.etClave.getText().toString();
